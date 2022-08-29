d2d0ee3c7fff200ea35dd9c9e98a759356d291c8
Sujana Bhandari <sujana6589@gmail.com>
2022-08-29 15:49:07 +0200

    Upload multiple images and display

--- controllers/users.js
+++ controllers/users.js
@@ -18,19 +18,16 @@ const  upload_profile_pics= async (req, res, next) => {
   const upload_cat_pics= async (req, res, next) => {
 
     console.log("FROM CATS", req.files);
-    req.files
-    // const { filename } = req.files;
-    // console.log(filename)
-    const result = req.files;
-    // result.map(photo => console.log(photo))
-
-    // const targetPath = "." + destination + filename;
-    // console.log("hello", filename)
+    const results = req.files;
+  //   let final=[]
+  //   for (let index = 0; index < results.length; index++) {
+  //     final = results[index].filename;
+  //  }
     try {
-      return res.status(201).send("hello");
-      // return res.status(201).send(`<h2>Here is the picture:</h2><img src="${filename}"  alt="Image"/>`);
-      
-    } catch (err) {
+      return res.status(201).send( `<h2>Here is the picture:</h2>
+      ${results.map(cat => `<img src="${cat.filename}" alt=”something”/><br />`)}`,);   
+    } 
+    catch (err) {
       console.log(err, "Some error");
       next(err);
     }
