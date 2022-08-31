fd58df9ba632736680348b845c61732fe6873f5c
Sujana Bhandari <sujana6589@gmail.com>
2022-08-31 14:00:17 +0200

    Filter the id and display the images based on id

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 const upload = require("../middlewares/image_storage");
 
 const {
-  upload_profile_pics, upload_cat_pics, get_all_pics, get_all_pics_dabase
+  upload_profile_pics, upload_cat_pics, get_all_pics, get_all_pics_dabase, get_all_pics_dabase_id
 } = require("../controllers/users");
 /* GET home page. */
 
@@ -27,6 +27,10 @@ router
 router
 .route('/get-pics')
 .get(get_all_pics_dabase)
+
+router
+.route('/get-pics/:id')
+.get(get_all_pics_dabase_id)
 
 router
   .route('/upload-cat-pics')
